74814283183d4e42bd673d45bece930f6bf0ef60
Janamajaya Mall <janmajaya.choithram@gmail.com>
2019-03-14 04:45:47 +0800

    finished task 3

--- handleCategoryDisplay.php
+++ handleCategoryDisplay.php
@@ -4,47 +4,11 @@
     //Select database
     mysqli_select_db($conn, 'jmall') or die('Error! '. mysqli_error($conn));
     //Construct your SQL query here 
-    $query = 'select * from catalog';
-    if ($_GET["category"]=="Books"){
-        $subQuery = ' WHERE `itemCategory` = \''.$_GET["category"].'\'';
-        $totalQuery = $query.$subQuery;
-    }else if ($_GET["category"]=="Laptops"){
-        $subQuery = ' WHERE `itemCategory` = \''.$_GET["category"].'\'';
-        $totalQuery = $query.$subQuery;
-    }else if ($_GET["category"]=="Speakers"){
-        $subQuery = ' WHERE `itemCategory` = \''.$_GET["category"].'\'';
-        $totalQuery = $query.$subQuery;
-    }
+    $query = 'select distinct itemCategory from catalog;';
     $result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
+    $newArray = array();
     while($row = mysqli_fetch_array($result)) {
-        print       "<tr>
-                        <td>
-                            <img id=\"shopping-cart-icon\" src=\"".$row['itemImage']."\" alt=\"Shopping Cart\">
-                        </td>
-                        <td>
-                            <table>
-                            <tr>
-                                <p>
-                                    ".$row['itemName']."
-                                </p>
-                            </tr>
-                            <tr>
-                                <p>
-                                    ".$row['itemPrice']."
-                                </p>
-                            </tr>
-                            <tr>
-                                <p>
-                                    ".$row['itemDescription']."
-                                </p>
-                            </tr>
-                            <tr>
-                                <p>
-                                    Add to Cart
-                                </p>
-                            </tr>
-                        </table>
-                        </td>
-                    </tr>";
+        array_push($newArray, $row['itemCategory']);
     }
+    print json_encode($newArray);
 ?>
